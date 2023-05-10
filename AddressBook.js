7b4791c2b9e97ddea876f7b17975e9623ce5e2a5
akhilmondal <akhilmandalfamily@gmail.com>
2023-05-10 20:52:17 +0530

    Added: Program to validate the contact.

--- AddressBook.js
+++ AddressBook.js
@@ -1,23 +1,54 @@
 console.log("Welcome to AddressBook System with JavaScript ")
 
+// create an array to hold the contacts
 let addressBook = [];
-// constructor function to create contact objects
-class Contact {
-    constructor(firstName, lastName, address, city, state, zip, phone, email) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.city = city;
-        this.state = state;
-        this.zip = zip;
-        this.phone = phone;
-        this.email = email;
-    }
+
+// create a constructor function to create contact objects
+function Contact(firstName, lastName, address, city, state, zip, phone, email) {
+  // validate the inputs
+  if (!/^[A-Z][a-z]{2,}$/.test(firstName)) {
+    throw new Error("Invalid first name. Must start with capital letter and have at least 3 characters.");
+  }
+  if (!/^[A-Z][a-z]{2,}$/.test(lastName)) {
+    throw new Error("Invalid last name. Must start with capital letter and have at least 3 characters.");
+  }
+  if (!/^[A-Za-z0-9\s]{4,}$/.test(address)) {
+    throw new Error("Invalid address. Must have at least 4 characters.");
+  }
+  if (!/^[A-Za-z\s]{4,}$/.test(city)) {
+    throw new Error("Invalid city. Must have at least 4 characters.");
+  }
+  if (!/^[A-Z]{2}$/.test(state)) {
+    throw new Error("Invalid state. Must be a 2-letter abbreviation.");
+  }
+  if (!/^\d{6}$/.test(zip)) {
+    throw new Error("Invalid ZIP code. Must be a 5-digit number.");
+  }
+  if (!/^\d{10}$/.test(phone)) {
+    throw new Error("Invalid phone number. Must be in the format xxxxxxxxxx.");
+  }
+  if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+    throw new Error("Invalid email address.");
+  }
+
+  // if all inputs are valid, create the contact object
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.address = address;
+  this.city = city;
+  this.state = state;
+  this.zip = zip;
+  this.phone = phone;
+  this.email = email;
 }
 
 // add some sample contacts to the address book
-addressBook.push(new Contact("Akhil", "Mondal", "Doddabalapur", "Karnataka", "Karnataka", "847563", "9348516846", "akhilmandalfamily@gmail.com"));
-addressBook.push(new Contact("Abhi", "Mondal", "Malkangiri", "Malkangiri", "Odisha", "764086", "7749935811", "abhijeet@gmail.com"));
+try {
+    addressBook.push(new Contact("Akhil", "Mondal", "Doddabalapur", "Karnataka", "KR", "847563", "9348516846", "akhilmandalfamily@gmail.com"));
+    addressBook.push(new Contact("Abhi", "Mondal", "Malkangiri", "Malkangiri", "OD", "764086", "7749935811", "abhijeet@gmail.com"));
+} catch (error) {
+  console.log(error.message);
+}
 
 // display the contents of the address book
 for (let i = 0; i < addressBook.length; i++) {
